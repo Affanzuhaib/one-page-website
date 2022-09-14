4b556fdb65b9e35c02c3e9dee082c54dc024fa52
Affanzuhaib <affanzuhaibuddin@gmail.com>
2022-09-14 23:37:56 +0530

    Updates

---
+++ precache-manifest.62e122f3f17792008f49c280a516e15a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "574f3dcf6c769433fbfd",
+    "url": "/affan_portfolio/static/css/main.9845729b.chunk.css"
+  },
+  {
+    "revision": "574f3dcf6c769433fbfd",
+    "url": "/affan_portfolio/static/js/main.574f3dcf.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/affan_portfolio/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/affan_portfolio/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "5c247aac95ecab90f2fd",
+    "url": "/affan_portfolio/static/js/runtime~main.5c247aac.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/affan_portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "daf624e4a5dc0df220e6623e720ac521",
+    "url": "/affan_portfolio/index.html"
+  }
+];
